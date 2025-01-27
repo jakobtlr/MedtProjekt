b505d3f3ce016a09a1e41718cb005c1e64fa8bf6
jakobtlr <jak.tiller@gmail.com>
2025-01-27 08:19:09 +0100

    vl gehts jz

--- threejs/src/app/components/main-project/main-project.component.ts
+++ threejs/src/app/components/main-project/main-project.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
 import * as THREE from 'three';
 import {
   BufferGeometry,
@@ -10,9 +10,9 @@ import {
   WebGLRenderer,
   SpotLight
 } from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
-import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader';
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
+import {OBJLoader} from 'three/examples/jsm/loaders/OBJLoader';
+import {MTLLoader} from 'three/examples/jsm/loaders/MTLLoader';
 
 @Component({
   selector: 'app-main-project',
@@ -27,36 +27,39 @@ export class MainProjectComponent implements AfterViewInit {
   renderer!: WebGLRenderer;
   clock: Clock = new Clock();
   controls!: OrbitControls;
-  map!: Mesh;
-  spotLight: SpotLight =  new THREE.SpotLight(0xffffff, 300);
+  spotLight: SpotLight = new THREE.SpotLight(0xffffff, 5000);
   ghast!: THREE.Object3D;
   ghastOrbitRadius = 30;
   ghastOrbitSpeed = 0.4;
   ghastOrbitAngle = 0;
   portalPosition = new THREE.Vector3(381, 0, 430);
+  moon!: Mesh;
+  map!: Mesh;
 
-  constructor() {}
+  constructor() {
+  }
 
   ngAfterViewInit(): void {
     this.scene = new THREE.Scene();
     this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    this.renderer = new THREE.WebGLRenderer({ canvas: this.canvas.nativeElement });
+    this.renderer = new THREE.WebGLRenderer({canvas: this.canvas.nativeElement});
     this.renderer.setSize(window.innerWidth, window.innerHeight);
     this.renderer.shadowMap.enabled = true;
 
-    this.spotLight.position.set(381, 175, 420);
+    this.spotLight.position.set(381, 100, 430);
     this.spotLight.target.position.copy(this.portalPosition);
     this.spotLight.castShadow = true;
+
     this.scene.add(this.spotLight);
     this.scene.add(this.spotLight.target);
 
     const loader = new THREE.TextureLoader();
     loader.load('assets/textures/HeightMapPaint.png', (texture) => this.onTextureLoaded(texture));
 
-    this.camera.position.set(380, 100, 430);
+    this.camera.position.set(380, 400, 430);
     this.camera.lookAt(381, 0, 400);
 
-    const ambientLight = new THREE.AmbientLight(0x404040, 0.1);
+    const ambientLight = new THREE.AmbientLight(0xffffff, 0.1);
     this.scene.add(ambientLight);
 
     this.controls = new OrbitControls(this.camera, this.renderer.domElement);
@@ -64,19 +67,41 @@ export class MainProjectComponent implements AfterViewInit {
     this.controls.dampingFactor = 0.25;
     this.controls.screenSpacePanning = true;
 
+    this.loadMoon();
     this.loadGhast();
     this.loadPortal();
 
     this.renderer.setAnimationLoop(() => this.animate());
   }
 
+  loadMoon(): void {
+    const textureLoader = new THREE.TextureLoader();
+
+    const normalMap = textureLoader.load('assets/textures/moonnormal.jpg');
+    const moonGeometry = new THREE.SphereGeometry(30, 30, 30);
+    const moonMaterial = new THREE.MeshStandardMaterial({
+      color: 0xF6F1D5,
+      normalMap: normalMap,
+    });
+
+    moonMaterial.normalScale.set(3, 3);
+
+    this.moon = new THREE.Mesh(moonGeometry, moonMaterial);
+    this.moon.position.set(381, 300, 430);
+    this.moon.receiveShadow = true;
+    this.moon.castShadow = true;
+
+    this.scene.add(this.moon);
+  }
+
+
   loadPortal(): void {
     const mtlLoader = new MTLLoader();
-    mtlLoader.load('assets/textures/Portal/Portal0000.mtl', (materials) => {
+    mtlLoader.load('assets/textures/Portal.mtl', (materials) => {
       materials.preload();
       const objLoader = new OBJLoader();
       objLoader.setMaterials(materials);
-      objLoader.load('assets/textures/Portal/Portal0000.obj', (object) => {
+      objLoader.load('assets/textures/Portal.obj', (object) => {
         object.traverse((child) => {
           if ((child as THREE.Mesh).isMesh) {
             const mesh = child as THREE.Mesh;
